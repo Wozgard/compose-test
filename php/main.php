a763bd1db03acaebbb76e940a38527e62d08914b
Wozgard <wozgard@gmail.com>
2022-08-22 14:30:49 +0300

    add index.html & create connection html+php

--- php/main.php
+++ php/main.php
@@ -1,19 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <h1 style="font-size: 50px;">AAAAGGGGGGGGAAAA</h1>
-    <br>
-    <hr>
-    <a href="/php.php">PHP API</a>
-    <br>
-    <a href="/main.php">main</a>
-    <br>
-    <a href="/main">PHP LOCAL</a>
+    <?php
+    echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+    ?>
 </body>
+
 </html>
